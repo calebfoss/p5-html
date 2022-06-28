c9a45812bd6dff6b2e76d0d6578bb42bb1626d9f
Glitch (glitch-blank-website) <none>
2022-06-28 20:45:13 +0000

    👽🍚 Checkpoint ./p5-html.js:8580995/6087 ./index.html:8580995/1129 ./p5.js:8580995/4253053 ./p5.sound.min.js:8580995/200064

---
+++ p5-html.js
@@ -0,0 +1,230 @@
+(() => {
+  class P5El extends HTMLElement {
+    constructor() {
+      super();
+      els.push(this);
+    }
+  }
+
+  class P5Function extends P5El {
+    #params;
+
+    constructor(overloads) {
+      super();
+
+      this.#params = [];
+      let overloadMatch = false;
+      for (const i in overloads) {
+        const overloadParams = overloads[i].split(",").map((s) => s.trim());
+        //  Check every required parameter has an attribute
+        overloadMatch = overloadParams.every(
+          (p) =>
+            this.hasAttribute(p) ||
+            (p.slice(0, 1) === "[" && p.slice(-1) === "]")
+        );
+        //  If matched overload found
+        if (overloadMatch) {
+          //  Save parameters with attributes
+          this.#params = overloadParams.filter((p) => this.hasAttribute(p));
+          //  Create getter for each attribute
+          this.#params.forEach((param) =>
+            Object.defineProperty(this, param, {
+              get: () => this.getAttribute(param),
+            })
+          );
+          break;
+        }
+      }
+      if (!overloadMatch)
+        console.error(
+          `No overloads for ${this.fnName} match provided parameters:`,
+          this.attributes
+        );
+    }
+    get codeString() {
+      return `${this.fnName}(${this.#params.map((p) => this.getAttribute(p))})`;
+    }
+    get fnName() {
+      return this.constructor.name.toLowerCase();
+    }
+  }
+
+  class P5ColorFunction extends P5Function {
+    constructor(overloads) {
+      overloads = [
+        "v1, v2, v3, [alpha]",
+        "value",
+        "gray, [alpha]",
+        "values",
+        "color",
+        ...overloads,
+      ];
+      super(overloads);
+    }
+  }
+
+  class Primitive2D extends P5Function {
+    static colorProps = ["fill", "noFill", "noStroke", "stroke"];
+    static transProps = [
+      "translate",
+      "rotate",
+      "rotateX",
+      "rotateY",
+      "rotateZ",
+      "scale",
+      "shearX",
+      "shearY",
+    ];
+
+    constructor(overloads) {
+      super(overloads);
+    }
+    get codeString() {
+      const colorString = Primitive2D.colorProps
+        .filter((prop) => this.hasAttribute(prop))
+        .map((prop) => `${prop}(${this.getAttribute(prop)})`)
+        .join(";");
+      const transString = Primitive2D.transProps
+        .filter((prop) => this.hasAttribute(prop))
+        .map((prop) => `${prop}(${this.getAttribute(prop)})`)
+        .join(";");
+      return `push();
+      ${colorString};
+      ${transString};
+      ${super.codeString};
+      pop()`;
+    }
+  }
+  const els = [
+    class Sketch extends P5Function {
+      constructor() {
+        const overloads = ["width, height, [renderer]"];
+        super(overloads);
+      }
+      get codeString() {
+        return Array.from(this.children)
+          .map((c) => c.drawString)
+          .join(";");
+      }
+    },
+    class Iterate extends P5Function {
+      constructor() {
+        super(["count", "init, test, update"]);
+      }
+      get codeString() {
+        const init = this.init || "let i = 0";
+        const test = this.test || `i < ${this.count}`;
+        const update = this.update || "i++";
+        const childCode = Array.from(this.children)
+          .map((c) => c.codeString)
+          .join(";");
+        return `for(${init}; ${test}; ${update}) {
+          ${childCode}
+        }`;
+      }
+    },
+
+    class State extends P5El {
+      constructor() {
+        super();
+      }
+      get codeString() {
+        return Array.from(this.attributes)
+          .map((a) => `${a.name} = ${this.getAttribute(a.name)}`)
+          .join(";");
+      }
+    },
+
+    class Background extends P5ColorFunction {
+      constructor() {
+        const overloads = ["colorstring, [a]", "gray, [a]", "v1, v2, v3, [a]"];
+        super(overloads);
+      }
+    },
+
+    class Arc extends Primitive2D {
+      constructor() {
+        const overloads = [
+          "x, y, w, h, start, stop, [mode], [detail], image, [a]",
+        ];
+        super(overloads);
+      }
+    },
+    class Ellipse extends Primitive2D {
+      constructor() {
+        const overloads = ["x, y, w, [h]", "x, y, w, h, [detail]"];
+        super(overloads);
+      }
+    },
+    class Circle extends Primitive2D {
+      constructor() {
+        const overloads = ["x, y, d"];
+        super(overloads);
+      }
+    },
+    class Line extends Primitive2D {
+      constructor() {
+        const overloads = ["x1, y1, x2, y2", "x1, y1, z1, x2, y2, z2"];
+        super(overloads);
+      }
+    },
+    class Point extends Primitive2D {
+      constructor() {
+        const overloads = ["x, y, [z]", "coordinate_vector"];
+        super(overloads);
+      }
+    },
+    class Quad extends Primitive2D {
+      constructor() {
+        const overloads = [
+          "x1, y1, x2, y2, x3, y3, x4, y4, [detailX], [detailY]",
+          "x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4, [detailX], [detailY]",
+        ];
+        super(overloads);
+      }
+    },
+    class Rect extends Primitive2D {
+      constructor() {
+        const overloads = [
+          "x, y, w, [h], [tl], [tr], [br], [bl]",
+          "x, y, w, h, [detailX], [detailY]",
+        ];
+        super(overloads);
+      }
+    },
+    class Square extends Primitive2D {
+      constructor() {
+        const overloads = ["x, y, s, [tl], [tr], [br], [bl]"];
+        super(overloads);
+      }
+    },
+    class Triangle extends Primitive2D {
+      constructor() {
+        const overloads = ["x1, y1, x2, y2, x3, y3"];
+        super(overloads);
+      }
+    },
+  ];
+  for (const i in els) {
+    customElements.define(`p5-${els[i].name.toLowerCase()}`, els[i]);
+  }
+})();
+
+const sketch = document.querySelector("p5-sketch");
+
+function setup() {
+  createCanvas(sketch.width, sketch.height).parent(sketch);
+  console.log("HERE");
+}
+
+function draw() {
+  const codeString = Array.from(sketch.children)
+    .map((c) => c.codeString)
+    .join(";");
+  Function(codeString)();
+  for (let i = 0; i < sketch.children.length; i++) {
+    if (sketch.children[i].hasAttribute("self-destruct")) {
+      sketch.children[i].remove();
+    }
+  }
+}
